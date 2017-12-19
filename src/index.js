c44db357e1b83962a223820c5baf78d8c145109b
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-19 06:16:59 -0700

    Build out rootReducer index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import App from './components/App/App';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools, applyMiddleware(thunk, logger));
+const router = (
+  <Provider store = {store} >
+    <BrowserRouter>
+      <App store={store}/>
+    </BrowserRouter>
+  </Provider>
+);
+
+ReactDOM.render(router, document.getElementById('root'));
 registerServiceWorker();
