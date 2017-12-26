1f8128488182eb53026b2aefaaec86b2158d4779
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-26 11:31:56 -0700

    Build initial Card test and bare component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Card.css';
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="Card">
+        I am an Card
+      </div>
+    );
+  }
+}
+
+export default Card;
