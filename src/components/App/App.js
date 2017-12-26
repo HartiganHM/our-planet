d17ca43343be6be287efe91e230c10de54481153
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-26 16:25:54 -0700

    Convert App to component and import Header and Routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import Header from '../Header/Header';
+import Routes from '../Routes/Routes';
 import './App.css';
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
       <div className="App">
-        I am an app
+        <Header />
+        <Routes />
       </div>
     );
-  }
 }
 
 export default App;
