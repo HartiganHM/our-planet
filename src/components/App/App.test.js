d57f50875c538eb0edea3740ffd6962bb058c52d
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-26 11:28:27 -0700

    Build initial App test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  
+describe('App tests', () => {
+  it('Should exist', () => {
+    const renderedApp = shallow(<App />);
+    expect(renderedApp).toBeDefined();
+  });
 });
