db1d8ede7977f417627e5e1a5634b1233054849f
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-27 16:28:30 -0700

    Complete fetchAnimalsData tests

--- src/helpers/fetchAnimalsData/fetchAnimalsData.test.js
+++ src/helpers/fetchAnimalsData/fetchAnimalsData.test.js
@@ -1,20 +1,38 @@
 import fethcAnimalsData from './fetchAnimalsData';
 import mockAnimalsArray from '../../data/mockAnimalsArray';
+import fetchAnimalsData from './fetchAnimalsData';
+
+window.fetch = jest.fn().mockImplementation(() =>
+  Promise.resolve({
+    status: 200,
+    json: () => Promise.resolve(mockAnimalsArray)
+  })
+);
 
 describe('fetchAnimalsData tests', () => {
+  let mockAnimalsData;
+
+  beforeEach( async () => {
+    mockAnimalsData = await fetchAnimalsData();
+  });
+
   it('Should be called correctly', () => {
-
+    expect(fetchAnimalsData).toBeAFunction;
   });
 
-  it('Should be called with the correct parameters', () => {
-
+  it('Should return an object when called', async () => {
+    expect(typeof mockAnimalsData).toEqual('object');
   });
 
-  it('Should return an object when called', () => {
+  it('Should throw an error if the fetch fails', async () => {
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.reject({
+        status: 500
+      })
+    );
+    const mockAnimalsData = await fethcAnimalsData();
+    const expected = Error('Error fetching animals data: [object Object]');
 
-  });
-
-  it('Should throw an error if the fetch fails', () => {
-
+    expect(mockAnimalsData).toEqual(expected)
   });
 });
