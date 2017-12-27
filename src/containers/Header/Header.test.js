a683cb9b06093664fe28d5b11a0d9c1667940917
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-27 15:52:52 -0700

    Pass initial Header tests

--- src/containers/Header/Header.test.js
+++ src/containers/Header/Header.test.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import { Header, mapDispatchToProps } from './Header';
+import * as actions from '../../actions';
 
 describe('Header tests', () => {
-  it('Should exist', () => {
+  it('Should match the snapshot', () => {
     const mockPopulateData = jest.fn();
-    const renderedHeader = shallow(<Header populateData={mockPopulateData}/>);
-    expect(renderedHeader).toBeDefined();
+    const renderedHeader = shallow(<Header populateData={mockPopulateData} />);
+    expect(renderedHeader).toMatchSnapshot();
+  });
+
+  describe('Header mapDispatchToProps tests', () => {
+    it('Should call dispatch when populateData is called', () => {
+      const mockDispatch = jest.fn();
+      const result = mapDispatchToProps(mockDispatch);
+
+      result.populateData();
+      expect(mockDispatch).toHaveBeenCalled();
+    });
   });
 });
