0a0e0cbf298f5091e9bb955a8b4f2168bdf6d0e1
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-27 16:21:59 -0700

    Build try catch for error safe guard

--- src/helpers/fetchAnimalsData/fetchAnimalsData.js
+++ src/helpers/fetchAnimalsData/fetchAnimalsData.js
@@ -1,7 +1,12 @@
 const fetchAnimalsData = async () => {
-  const fetchedData = await fetch('http://localhost:3001/api/v1/animals');
-  const jsonData = await fetchedData.json();
-  return jsonData
-}
+  try {
+    const fetchedData = await fetch('http://localhost:3001/api/v1/animals');
+    const jsonData = await fetchedData.json();
+    return jsonData;
+
+  } catch (error) {
+    return new Error(`Error fetching animals data: ${error}`);
+  }
+};
 
 export default fetchAnimalsData;
