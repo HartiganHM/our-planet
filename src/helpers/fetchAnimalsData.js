16cd60c7f5b96c4a149786696998eb21bac9ea15
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-26 17:51:46 -0700

    Add missing async to functions

--- src/helpers/fetchAnimalsData.js
+++ src/helpers/fetchAnimalsData.js
@@ -1,4 +1,4 @@
-const fetchAnimalsData = () => {
+const fetchAnimalsData = async () => {
   const fetchedData = await fetch('http://localhost:3001/api/v1/animals')
   const jsonData = await fetchedData.json();
   return jsonData
--- src/helpers/fetchContinentsData.js
+++ src/helpers/fetchContinentsData.js
@@ -1,4 +1,4 @@
-const fetchAnimalsData = () => {
+const fetchAnimalsData = async () => {
   const fetchedData = await fetch('http://localhost:3001/api/v1/continents')
   const jsonData = await fetchedData.json();
   return jsonData
