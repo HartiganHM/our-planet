2853b2150cf7179a2dac536213e56df6b88a60c1
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-27 16:13:59 -0700

    Mock out expected tests for fetchAnimalsData

---
+++ src/helpers/fetchAnimalsData/fetchAnimalsData.test.js
@@ -0,0 +1,20 @@
+import fethcAnimalsData from './fetchAnimalsData';
+import mockAnimalsArray from '../../data/mockAnimalsArray';
+
+describe('fetchAnimalsData tests', () => {
+  it('Should be called correctly', () => {
+
+  });
+
+  it('Should be called with the correct parameters', () => {
+
+  });
+
+  it('Should return an object when called', () => {
+
+  });
+
+  it('Should throw an error if the fetch fails', () => {
+
+  });
+});
