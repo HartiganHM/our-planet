50c9ca9e7bb58204a72b11efdf5f458a248e93b1
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-27 06:18:35 -0700

    Fix typos

--- src/helpers/fetchAnimalsData.js
+++ src/helpers/fetchAnimalsData.js
@@ -1,5 +1,5 @@
 const fetchAnimalsData = async () => {
-  const fetchedData = await fetch('http://localhost:3001/api/v1/animals')
+  const fetchedData = await fetch('http://localhost:3001/api/v1/animals');
   const jsonData = await fetchedData.json();
   return jsonData
 }
--- src/helpers/fetchContinentsData.js
+++ src/helpers/fetchContinentsData.js
@@ -1,7 +1,7 @@
-const fetchAnimalsData = async () => {
+const fetchContinentsData = async () => {
   const fetchedData = await fetch('http://localhost:3001/api/v1/continents')
   const jsonData = await fetchedData.json();
   return jsonData
 }
 
-export default fetchAnimalsData;
+export default fetchContinentsData;
