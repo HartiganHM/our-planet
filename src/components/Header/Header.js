efa7e4524447092f1a580a012722e3683922c337
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-26 17:11:27 -0700

    Build initial NavLinks for Home and About

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 class Header extends Component {
   render() {
     return (
       <div className="Header">
-        I am an Header
+        <NavLink to="/">
+          <img src="../images/Our-Planet-Text.png" alt="our-planet-wordmark" />
+        </NavLink>
+        <NavLink to="/about">About</NavLink>
       </div>
     );
   }
