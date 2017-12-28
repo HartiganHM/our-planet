d1ccf7c5878f6cb3dfb35f4fba42adbcd44fd576
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-28 14:33:57 -0700

    Mock out addImageToAnimal tests

---
+++ src/helpers/addImageToAnimal/addImageToAnimal.test.js
@@ -0,0 +1,15 @@
+import addImageToAnimal from './addImageToAnimal';
+
+describe('addImageToAnimal tests', () => {
+  it('Should be a function', () => {
+
+  });
+
+  it('Should return an array', () => {
+
+  });
+
+  it('Should add an image matching each animals name', () => {
+    
+  })
+})
