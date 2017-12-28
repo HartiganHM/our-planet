06880b8dfbd5af1897c500b513bd745de480fa01
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-27 18:10:59 -0700

    Mock out anticipated continentsReducers tests

---
+++ src/reducers/continents/continentsReducers.test.js
@@ -0,0 +1,13 @@
+import continentsReducers from './continentsReducers';
+import mockContinentsArray from '../../data/mockContinentsArray';
+import * as actions from '../../actions';
+
+describe('continentsReducers tests', () => {
+  it('Should return store by default', () => {
+
+  });
+
+  it('Should return a new store with continents', () => {
+
+  });
+});
