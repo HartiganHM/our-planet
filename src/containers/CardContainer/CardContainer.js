131e25ded545c1903d9f9a7e21792fd7b13d36bf
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-28 15:11:06 -0700

    Convert CardContainer to class

--- src/containers/CardContainer/CardContainer.js
+++ src/containers/CardContainer/CardContainer.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './CardContainer.css';
 
-const CardContainer = () => {
-  return <div className="CardContainer">I am an CardContainer</div>;
-};
+class CardContainer extends Component {
+  render() {
+    return <div className="CardContainer">I am an CardContainer</div>;
+  }
+}
 
 export default CardContainer;
