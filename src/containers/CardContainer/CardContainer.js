f921630791a4839cb48cd01e7828578ed74dd7ed
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-28 15:12:52 -0700

    Connect CardContainer to store

--- src/containers/CardContainer/CardContainer.js
+++ src/containers/CardContainer/CardContainer.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+import PropTypes from 'prop-types';
 import './CardContainer.css';
 
-class CardContainer extends Component {
+export class CardContainer extends Component {
   render() {
     return <div className="CardContainer">I am an CardContainer</div>;
   }
 }
 
-export default CardContainer;
+export const mapStateToProps = store => {
+  return {
+    animals: store.animals
+  };
+};
+
+export default connect(mapStateToProps, null)(CardContainer);
