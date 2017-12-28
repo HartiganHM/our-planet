37d84506dc092c29898740edf034e3111b4605b5
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-28 15:46:10 -0700

    Remove CL

--- src/containers/CardContainer/CardContainer.js
+++ src/containers/CardContainer/CardContainer.js
@@ -6,7 +6,6 @@ import PropTypes from 'prop-types';
 import './CardContainer.css';
 
 export const CardContainer = props => {
-  console.log(props)
   const animalCards = props.animals.map(animal => (
     <Card animalData={animal} />
   ));
