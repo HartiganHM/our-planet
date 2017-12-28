a1fdcdb7f9c73645d876e6916264c8a62dd4f448
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-27 17:46:59 -0700

    Complete animalsReducers test suite

--- src/reducers/animals/animalsReducers.test.js
+++ src/reducers/animals/animalsReducers.test.js
@@ -1,12 +1,22 @@
-import animalsReducer from './animalsReducers';
+import animalsReducers from './animalsReducers';
 import mockAnimalsArray from '../../data/mockAnimalsArray';
+import * as actions from '../../actions';
 
 describe('animalsReducers tests', () => {
   it('should return store by default', () => {
+    const expected = [];
 
+    expect(animalsReducers(undefined, {})).toEqual(expected);
   });
 
   it('Should return a new store with animals', () => {
-    
-  })
-})
+    const expected = {
+      animalsData: mockAnimalsArray,
+      type: 'STORE_ANIMAL_DATA'}
+      ;
+
+    expect(
+      animalsReducers(undefined, actions.storeAnimalData(mockAnimalsArray))
+    ).toEqual(expected);
+  });
+});
