04fa4cfd799d50258f6460700f0e3fc26e0ed242
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-27 17:03:25 -0700

    Mock out needed tests for animalsReducers

---
+++ src/reducers/animals/animalsReducers.test.js
@@ -0,0 +1,12 @@
+import animalsReducer from './animalsReducers';
+import mockAnimalsArray from '../../data/mockAnimalsArray';
+
+describe('animalsReducers tests', () => {
+  it('should return store by default', () => {
+
+  });
+
+  it('Should return a new store with animals', () => {
+    
+  })
+})
