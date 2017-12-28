5c4991dff85f4e47e51baf97639e63d0bd931d39
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-27 18:14:11 -0700

    Complete continentsReducers test suite

--- src/reducers/continents/continentsReducers.test.js
+++ src/reducers/continents/continentsReducers.test.js
@@ -4,10 +4,22 @@ import * as actions from '../../actions';
 
 describe('continentsReducers tests', () => {
   it('Should return store by default', () => {
+    const expected = [];
 
+    expect(continentsReducers(undefined, {})).toEqual(expected);
   });
 
   it('Should return a new store with continents', () => {
+    const expected = {
+      continentsData: mockContinentsArray,
+      type: 'STORE_CONTINENTS_DATA'
+    };
 
+    expect(
+      continentsReducers(
+        undefined,
+        actions.storeContinentsData(mockContinentsArray)
+      )
+    ).toEqual(expected);
   });
 });
