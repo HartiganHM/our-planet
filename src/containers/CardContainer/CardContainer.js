99783785995137e04343418e092765137d5d89a2
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-28 15:20:10 -0700

    Convert CardContainer to const

--- src/containers/CardContainer/CardContainer.js
+++ src/containers/CardContainer/CardContainer.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from '../Card/Card';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import PropTypes from 'prop-types';
 import './CardContainer.css';
 
-export class CardContainer extends Component {
-  render() {
-    const animalCards = this.props.animals.map(animal => (
-      <Card animalData={animal} />
-    ));
+export const CardContainer = props => {
+  console.log(props)
+  const animalCards = props.animals.map(animal => (
+    <Card animalData={animal} />
+  ));
 
-    return <div className="CardContainer">{animalCards}</div>;
-  }
-}
+  return <div className="CardContainer">{animalCards}</div>;
+};
 
 export const mapStateToProps = store => {
   return {
