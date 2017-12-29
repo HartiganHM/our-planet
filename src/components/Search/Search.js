e78fde77ec05f1938aee0ef97c3cf7e93b3802ae
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-29 10:01:20 -0700

    Adjust spacing

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Search.css';
 
 const Search = () => {
-  return <div className="Search"><input className='search-field' type='text' placeholder='Search' /></div>;
+  return (
+    <div className="Search">
+      <input className="search-field" type="text" placeholder="Search" />
+    </div>
+  );
 };
 
 export default Search;
