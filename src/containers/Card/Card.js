173c72f59da83de7b1c1c4afedaa3e1206778892
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-28 21:59:57 -0700

    Add propTypes and alt image tag

--- src/containers/Card/Card.js
+++ src/containers/Card/Card.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { PropTypes } from 'prop-types';
 import './Card.css';
 
 const Card = props => {
@@ -6,14 +7,19 @@ const Card = props => {
   const image = name.toLowerCase();
 
   return (
-    <div className="Card">
+    <div className='Card'>
       <img
-        className="animal-image"
+        className='animal-image'
         src={require(`../../images/animals/${image}.jpg`)}
+        alt={`${name}-endangered-species`}
       />
-      <span>{name}</span>
+      <span className='animal-name'>{name}</span>
     </div>
   );
 };
 
 export default Card;
+
+Card.propTypes = {
+  animalData: PropTypes.obj
+};
