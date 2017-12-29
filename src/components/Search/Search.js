5067e49adc09d32667ee9920be808a55fc5bd399
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-29 10:00:21 -0700

    Build basic input field

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Search.css';
 
 const Search = () => {
-  return <div className="Search">I am an Search</div>;
+  return <div className="Search"><input className='search-field' type='text' placeholder='Search' /></div>;
 };
 
 export default Search;
