e5564a4851a09acfe7752c979c6da9f209e2ca17
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-29 10:36:20 -0700

    Adjust spacing for readability

--- src/reducers/animals/animalsReducers.js
+++ src/reducers/animals/animalsReducers.js
@@ -2,9 +2,11 @@ const animalsReducers = (store = [], action) => {
   switch (action.type) {
   case 'STORE_ANIMAL_DATA':
     return action.animalsData;
+
   case 'SEARCH_ANIMALS':
     console.log(action.filteredAnimals)
     return action.filteredAnimals;
+
   default:
     return store;
   }
