51249732cc58ec2d9cbbcd69dae852512ec59cb4
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-29 10:23:56 -0700

    Connect Search to actions and send store to component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../../actions';
 import './Search.css';
 
-export const Search = () => {
+export const Search = props => {
+  const { searchAnimals, animals } = props;
+  console.log(animals)
+
   return (
     <div className="Search">
-      <input className="search-field" type="text" placeholder="Search" />
+      <input
+        className="search-field"
+        onChange={event => searchAnimals(event.target.value, animals)}
+        type="text"
+        placeholder="Search"
+        value=''
+      />
     </div>
   );
 };
 
+export const mapStateToProps = store => {
+  return {
+    animals: store.animals
+  }
+};
+
 export const mapDispatchToProps = dispath => {
   return {
-    
-  }
-}
+    searchAnimals: (inputValue, animals) =>
+      dispatchEvent(actions.filterAnimals(inputValue, animals))
+  };
+};
 
-export default connect(null, mapDispatchToProps)(Search);
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
