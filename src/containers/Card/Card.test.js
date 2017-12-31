7fa9ed87d65c92c9f387bab3b4a33af1038dcff4
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-31 13:04:15 -0700

    Complete Card test suite

--- src/containers/Card/Card.test.js
+++ src/containers/Card/Card.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Card from './Card';
+import mockAnimalsArray from '../../data/mockAnimalsArray';
 
 describe('Card tests', () => {
-  it('Should exist', () => {
-    const renderedCard = shallow(<Card />);
-    expect(renderedCard).toBeDefined();
+  it('Should match the snapshot', () => {
+    const mockAnimal = mockAnimalsArray[0];
+    const renderedCard = shallow(<Card animalData={mockAnimal} />);
+
+    expect(renderedCard).toMatchSnapshot();
+  });
+
+  it('Should have an image that matches the animals name', () => {
+    const mockAnimal = mockAnimalsArray[0];
+    const expected = 'arctic fox.jpg';
+    const renderedCard = shallow(<Card animalData={mockAnimal} />);
+
+    expect(renderedCard.find('img').prop('src')).toEqual(expected);
   });
 });
