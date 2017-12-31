8943202d812b74bff4d5f17d62843f009a99434c
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-31 14:02:51 -0700

    Complete filterAnimals test suite

--- src/helpers/filterAnimals/filterAnimals.test.js
+++ src/helpers/filterAnimals/filterAnimals.test.js
@@ -3,22 +3,27 @@ import mockAnimalsArray from '../../data/mockAnimalsArray';
 
 describe('filterAnimals tests', () => {
   it('Should be a function', () => {
-    
+    expect(filterAnimals).toBeAFunction;
   });
 
   it('Should return an object', () => {
-
-  });
-
-  it('Should be called with the correct parameters', () => {
-
+    expect(typeof filterAnimals('a', mockAnimalsArray)).toEqual('object');
   });
 
   it('Should should change an animals display property based on the input value', () => {
+    const expectedDisplayMatching = true;
+    const expectedDisplayNonMatching = false;
+    const mockFilteredAnimals = filterAnimals('fox', mockAnimalsArray);
 
+    expect(mockFilteredAnimals[0].display).toEqual(expectedDisplayMatching);
+    expect(mockFilteredAnimals[1].display).toEqual(expectedDisplayNonMatching);
   });
 
-  it('Should return an empty array if no animals match the input value', () => {
+  it('Should set all display properties to false if no animals match the input value', () => {
+    const mockFilteredAnimals = filterAnimals('whale', mockAnimalsArray);
+    const expected = false;
 
+    expect(mockFilteredAnimals[0].display).toEqual(expected);
+    expect(mockFilteredAnimals[1].display).toEqual(expected);
   });
 });
