b77dd3c64f4d04ece174ed582912e39f86b67340
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-31 13:38:59 -0700

    Mock out expected filterAnimals tests

---
+++ src/helpers/filterAnimals/filterAnimals.test.js
@@ -0,0 +1,24 @@
+import filterAnimals from './filterAnimals';
+import mockAnimalsArray from '../../data/mockAnimalsArray';
+
+describe('filterAnimals tests', () => {
+  it('Should be a function', () => {
+    
+  });
+
+  it('Should return an object', () => {
+
+  });
+
+  it('Should be called with the correct parameters', () => {
+
+  });
+
+  it('Should should change an animals display property based on the input value', () => {
+
+  });
+
+  it('Should return an empty array if no animals match the input value', () => {
+
+  });
+});
