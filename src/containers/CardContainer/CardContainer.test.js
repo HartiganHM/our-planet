1b7fc3d692176a3e2974dd71baa8f37723cb48a6
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-31 11:45:15 -0700

    Complete CardContainer test suite

--- src/containers/CardContainer/CardContainer.test.js
+++ src/containers/CardContainer/CardContainer.test.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import CardContainer from './CardContainer';
+import { CardContainer, mapStateToProps } from './CardContainer';
+import mockAnimalsArray from '../../data/mockAnimalsArray';
+import { mapDispatchToProps } from '../Header/Header';
 
 describe('CardContainer tests', () => {
-  it('Should exist', () => {
-    const renderedCardContainer = shallow(<CardContainer />);
-    expect(renderedCardContainer).toBeDefined();
+  let renderedCardContainer;
+
+  beforeEach(() => {
+    renderedCardContainer = shallow(
+      <CardContainer animals={mockAnimalsArray} />
+    );
+  });
+
+  it('Should match the snapshot', () => {
+    expect(renderedCardContainer).toMatchSnapshot();
+  });
+
+  it('Should display Cards', () => {
+    expect(renderedCardContainer.find('Card').length).toEqual(2);
+  });
+
+  it('Should not render certain cards if display is false', () => {
+    mockAnimalsArray[0].display = false;
+    renderedCardContainer = shallow(
+      <CardContainer animals={mockAnimalsArray} />
+    );
+
+    expect(renderedCardContainer.find('Card').length).toEqual(1);
+  })
+
+  describe('mapStateToProps tests', () => {
+    it('Should pull animals from the store', () => {
+      const mockStore = {
+        animals: mockAnimalsArray
+      };
+      const result = mapStateToProps(mockStore);
+
+      expect(result.user).toEqual(mockStore.user);
+    })
   });
 });
