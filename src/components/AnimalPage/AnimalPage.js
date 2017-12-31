4e3b0b1aee6ae1279fce899926e75c602a26b45f
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-31 16:31:50 -0700

    Connect AnimalPage to store and pull animal data based on match from route

--- src/components/AnimalPage/AnimalPage.js
+++ src/components/AnimalPage/AnimalPage.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './AnimalPage.css';
+import { mapDispatchToProps } from '../../containers/Header/Header';
 
-const AnimalPage = () => {
-  return <div className="AnimalPage">I am animalPage</div>;
+export const AnimalPage = (props) => {
+  const linkedAnimal = props.match.params.animal;
+  const match = props.animals.find(animal => animal.name === linkedAnimal);
+  const { name, status, population, scientific_name, height, weight, length, habitat, facts, human_benefit } = match;
+  const image = name.toLowerCase();
+
+  console.log(match)
+  return (
+    <div className="AnimalPage">
+      <img src={require(`../../images/animals/${image}.jpg`)} />
+    </div>);
 };
 
-export default AnimalPage;
+export const mapStateToProps = store => {
+  return {
+    animals: store.animals
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AnimalPage);
