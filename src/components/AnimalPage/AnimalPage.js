f1874b1ebf06e5df178febf03da64bd03ffd2d2d
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-31 17:01:12 -0700

    Convert AnimalPage to class and build componentWillReceiveProps

--- src/components/AnimalPage/AnimalPage.js
+++ src/components/AnimalPage/AnimalPage.js
@@ -1,25 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './AnimalPage.css';
 import { mapDispatchToProps } from '../../containers/Header/Header';
 
-export const AnimalPage = (props) => {
-  const linkedAnimal = props.match.params.animal;
-  const match = props.animals.find(animal => animal.name === linkedAnimal);
-  const { name, status, population, scientific_name, height, weight, length, habitat, facts, human_benefit } = match;
-  const image = name.toLowerCase();
+export class AnimalPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      animalData: {}
+    };
+  }
 
-  console.log(match)
-  return (
-    <div className="AnimalPage">
-      <img src={require(`../../images/animals/${image}.jpg`)} />
-    </div>);
-};
+  componentWillReceiveProps(nextProps) {
+    const linkedAnimal = this.props.match.params.animal;
+    const animalData = nextProps.animals.find(
+      animal => animal.name === linkedAnimal
+    );
+    animalData.image = animalData.name.toLowerCase();
+
+    this.setState({ animalData });
+  }
+
+  render() {
+    const {
+      name,
+      status,
+      population,
+      scientific_name,
+      height,
+      weight,
+      length,
+      habitat,
+      facts,
+      human_benefit,
+      image
+    } = this.state.animalData;
+    console.log(this.state.animalData);
+
+    return (
+      <div className="AnimalPage">
+        <span>{name}</span>
+        {image && (
+          <img
+            className="animal-image"
+            src={require(`../../images/animals/${image}.jpg`)}
+            alt={`${name}-endangered-species`}
+          />
+        )}
+      </div>
+    );
+  }
+}
 
 export const mapStateToProps = store => {
   return {
     animals: store.animals
-  }
-}
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(AnimalPage);
