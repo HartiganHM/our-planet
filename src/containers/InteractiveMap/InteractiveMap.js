7d94ec30c984100e0e08845c8ce39b5140793020
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-02 16:56:28 -0700

    Rename Map component to InteractiveMap due to keyword conflict; connect to state in store and render links

---
+++ src/containers/InteractiveMap/InteractiveMap.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { NavLink, Link } from 'react-router-dom';
+import * as actions from '../../actions';
+import PropTypes from 'prop-types';
+// import './Header.css';
+
+class InteractiveMap extends Component {
+  render() {
+    const buttons = this.props.continents.map(continent => {
+      const continentLink = '/' + continent.name;
+
+      return <Link to={continentLink}>{continent.name}</Link>;
+    });
+
+    return <div>{buttons}</div>;
+  }
+}
+
+export const mapStateToProps = store => {
+  return {
+    continents: store.continents
+  };
+};
+
+export default connect(mapStateToProps, null)(InteractiveMap);
