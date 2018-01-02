fc8b0c5a85083fdc199035fff48d8418f144c012
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-02 16:41:58 -0700

    Reset all animals on click of Header links

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import * as actions from '../../actions';
 import PropTypes from 'prop-types';
 import './Header.css';
@@ -11,7 +11,13 @@ export class Header extends Component {
   }
 
   renderLogo() {
-    return <div className="headerLogo" />;
+    return (
+      <Link
+        to="/"
+        className="headerLogo"
+        onClick={() => this.props.resetAnimals('', this.props.animals)}
+      />
+    );
   }
 
   render() {
@@ -19,10 +25,18 @@ export class Header extends Component {
       <div className="Header">
         {this.renderLogo()}
         <div className="nav-bar">
-          <NavLink className="nav-button" to="/">
+          <NavLink
+            className="nav-button"
+            to="/"
+            onClick={() => this.props.resetAnimals('', this.props.animals)}
+          >
             Home
           </NavLink>
-          <NavLink className="nav-button" to="/about">
+          <NavLink
+            className="nav-button"
+            to="/about"
+            onClick={() => this.props.resetAnimals('', this.props.animals)}
+          >
             About
           </NavLink>
         </div>
@@ -31,13 +45,21 @@ export class Header extends Component {
   }
 }
 
-export const mapDispatchToProps = dispatch => {
+export const mapStateToProps = store => {
   return {
-    populateData: () => dispatch(actions.fetchAllData())
+    animals: store.animals
   };
 };
 
-export default connect(null, mapDispatchToProps)(Header);
+export const mapDispatchToProps = dispatch => {
+  return {
+    populateData: () => dispatch(actions.fetchAllData()),
+    resetAnimals: (inputValue, animals) =>
+      dispatch(actions.searchAnimals(inputValue, animals))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
 
 Header.propTypes = {
   populateData: PropTypes.func
