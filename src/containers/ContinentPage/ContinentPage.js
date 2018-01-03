79fdd07caa67b4d10df95f0d03b98ac988182be1
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-02 17:07:10 -0700

    Convert ContinentsPage to class

--- src/containers/ContinentPage/ContinentPage.js
+++ src/containers/ContinentPage/ContinentPage.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './ContinentPage.css';
 
-const ContinentPage = () => {
-  return <div className="ContinentPage">I am an ContinentPage</div>;
+export class ContinentPage extends Component {
+  render() {
+    return <div className="ContinentPage">I am an ContinentPage</div>;
+  }
 };
 
 export default ContinentPage;
