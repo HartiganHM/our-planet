2d3a6cdbe3f3888bb7643f9bf25f06d6ea405332
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-03 12:49:40 -0700

    Mock out anticipated AnimalPage tests

--- src/containers/AnimalPage/AnimalPage.test.js
+++ src/containers/AnimalPage/AnimalPage.test.js
@@ -2,15 +2,33 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { AnimalPage, mapStateToProps } from './AnimalPage';
 import mockAnimalsArray from '../../data/mockAnimalsArray';
+import mockCleanAnimalData from '../../data/mockCleanAnimalData';
 import mockProps from '../../data/mockProps';
 
 describe('AnimalPage tests', () => {
-  it('Should exist', () => {
-    const renderedAnimalPage = shallow(
+  let renderedAnimalPage;
+
+  beforeEach(() => {
+    renderedAnimalPage = shallow(
       <AnimalPage animals={mockProps.animals} match={mockProps.match} />
     );
-    expect(renderedAnimalPage).toBeDefined();
   });
+
+  it('Should match the snapshot', () => {
+    expect(renderedAnimalPage).toMatchSnapshot();
+  });
+
+  it('cleanAnimalData should return an object', () => {});
+
+  it('cleanAnimalData should clean data properties', () => {});
+
+  it('renderAnimalStats should return an object', () => {});
+
+  it('renderAnimalStats should have an array for longStats and basicStats', () => {});
+
+  it('renderAnimalStats should not render empty properties', () => {});
+
+  it('longStats should container "The Facts" and "Why I\'m Important"', () => {});
 
   describe('mapStateToProps tests', () => {
     it('Should pull animals from the store', () => {
