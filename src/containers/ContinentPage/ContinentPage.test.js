3db7aa03e7ee9c2a41399bd6de716dff8d5629bf
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-03 14:37:07 -0700

    Complete initial test for ContinentPage

--- src/containers/ContinentPage/ContinentPage.test.js
+++ src/containers/ContinentPage/ContinentPage.test.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import ContinentPage from './ContinentPage';
+import { ContinentPage, mapStateToProps } from './ContinentPage';
+import mockContinentsArray from '../../data/mockContinentsArray';
+import mockProps from '../../data/mockProps';
 
 describe('ContinentPage tests', () => {
-  it('Should exist', () => {
-    const renderedContinentPage = shallow(<ContinentPage />);
-    expect(renderedContinentPage).toBeDefined();
+  let renderedContinentPage;
+
+  beforeEach(() => {
+    renderedContinentPage = shallow(
+      <ContinentPage
+        animals={mockProps.animals}
+        continents={mockProps.continents}
+        match={mockProps.match}
+      />
+    );
+  });
+
+  it('Should match the snapshot', () => {
+    expect(renderedContinentPage).toMatchSnapshot();
   });
 });
