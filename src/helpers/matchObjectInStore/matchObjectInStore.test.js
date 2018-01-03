b9f555196bb927f00db66992b08f3516a2316029
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-03 11:09:34 -0700

    Build out matchObjectInStore.test.js and anticipated tests

---
+++ src/helpers/matchObjectInStore/matchObjectInStore.test.js
@@ -0,0 +1,24 @@
+import matchObjectInStore from './matchObjectInStore';
+import mockAnimalsArray from '../../data/mockAnimalsArray';
+
+describe('matchObjectInStore tests', () => {
+  it('Should be a function', () => {
+
+  });
+
+  it('Should return an object', () => {
+
+  });
+
+  it('Should match an object from store based on props give', () => {
+
+  });
+
+  it('Should catch Antarctica outlier', () => {
+
+  });
+
+  it('should return an empty object if there are no matches', () => {
+
+  });
+})
