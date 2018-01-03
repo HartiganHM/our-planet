c02017559aac4a97143e483ca52c4e45eea37f03
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-03 10:21:35 -0700

    Build placeholder for continent with no endangered animals

--- src/containers/CardContainer/CardContainer.js
+++ src/containers/CardContainer/CardContainer.js
@@ -5,14 +5,25 @@ import PropTypes from 'prop-types';
 import './CardContainer.css';
 
 export const CardContainer = props => {
-  const animalsArray = props.continentAnimals ? props.continentAnimals : props.animals;
+  const noEndangeredAnimals = (
+    <span className="no-endangered-animals">
+      There are currently no endangered animals in this region
+    </span>
+  );
+  const animalsArray = props.continentAnimals
+    ? props.continentAnimals
+    : props.animals;
   const animalCards = animalsArray.map((animal, index) => {
     return animal.display
-      ? (<Card key={index} animalData={animal} />)
-      : (undefined);
+    ? (<Card key={index} animalData={animal} />)
+    : (undefined);
   });
 
-  return <div className="CardContainer">{animalCards}</div>;
+  return (
+    <div className="CardContainer">
+      {animalCards.length ? animalCards : noEndangeredAnimals}
+    </div>
+  );
 };
 
 export const mapStateToProps = store => {
