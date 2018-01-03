ad52271181d30776b1e422ff1de4baffac85a244
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-02 17:03:24 -0700

    Adjust Link pathname

--- src/containers/InteractiveMap/InteractiveMap.js
+++ src/containers/InteractiveMap/InteractiveMap.js
@@ -8,7 +8,7 @@ import PropTypes from 'prop-types';
 class InteractiveMap extends Component {
   render() {
     const buttons = this.props.continents.map(continent => {
-      const continentLink = '/' + continent.name;
+      const continentLink = '/continents/' + continent.name;
 
       return <Link to={continentLink}>{continent.name}</Link>;
     });
