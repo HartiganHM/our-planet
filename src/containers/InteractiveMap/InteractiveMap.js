d7f3254b8e232f2ef3af02ece60dd8fa1a7374c6
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-02 21:25:21 -0700

    Add key to map and PropTypes

--- src/containers/InteractiveMap/InteractiveMap.js
+++ src/containers/InteractiveMap/InteractiveMap.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { NavLink, Link } from 'react-router-dom';
-import * as actions from '../../actions';
+import { Link } from 'react-router-dom';
+// import * as actions from '../../actions';
 import PropTypes from 'prop-types';
 // import './Header.css';
 
 class InteractiveMap extends Component {
   render() {
-    const buttons = this.props.continents.map(continent => {
+    const buttons = this.props.continents.map((continent, index) => {
       const continentLink = '/continents/' + continent.name;
 
-      return <Link to={continentLink}>{continent.name}</Link>;
+      return (
+        <Link key={index} to={continentLink}>
+          {continent.name}
+        </Link>
+      );
     });
 
     return <div>{buttons}</div>;
@@ -24,3 +28,7 @@ export const mapStateToProps = store => {
 };
 
 export default connect(mapStateToProps, null)(InteractiveMap);
+
+InteractiveMap.propTypes = {
+  continents: PropTypes.array
+};
