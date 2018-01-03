6f15763a9444320e85055d13aa093cb0e35795c7
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-03 11:20:15 -0700

    Build mockProps and pass initial tests for matchObjectInStore

--- src/helpers/matchObjectInStore/matchObjectInStore.test.js
+++ src/helpers/matchObjectInStore/matchObjectInStore.test.js
@@ -1,24 +1,31 @@
 import matchObjectInStore from './matchObjectInStore';
 import mockAnimalsArray from '../../data/mockAnimalsArray';
+import mockContinentsArray from '../../data/mockContinentsArray';
 
 describe('matchObjectInStore tests', () => {
-  it('Should be a function', () => {
+  let mockProps;
 
+  beforeEach(() => {
+    mockProps = {
+      match: { params: { animal: 'Arctic Fox', continent: 'Antarctica ' } },
+      animals: mockAnimalsArray,
+      continents: mockContinentsArray
+    };
+  });
+
+  it('Should be a function', () => {
+    expect(matchObjectInStore).toBeAFunction;
   });
 
   it('Should return an object', () => {
+    const mockAnimal = matchObjectInStore(mockProps, 'animal', 'animals');
 
+    expect(typeof mockAnimal).toEqual('object');
   });
 
-  it('Should match an object from store based on props give', () => {
+  it('Should match an object from store based on props give', () => {});
 
-  });
+  it('Should catch Antarctica outlier', () => {});
 
-  it('Should catch Antarctica outlier', () => {
-
-  });
-
-  it('should return an empty object if there are no matches', () => {
-
-  });
-})
+  it('should return an empty object if there are no matches', () => {});
+});
