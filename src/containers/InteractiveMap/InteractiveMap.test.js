1f29c5a812c1c0d97617e955f8a02b753e50af64
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-03 15:16:25 -0700

    Mock out base level tests for InteractiveMap component

---
+++ src/containers/InteractiveMap/InteractiveMap.test.js
@@ -0,0 +1,18 @@
+import InteractiveMap from './InteractiveMap';
+import mockProps from '../../data/mockProps';
+
+describe('InteractiveMap tests', () => {
+  it('Should match the snapshot', () => {
+
+  });
+
+  it('Should desiplay a link for each continent', () => {
+
+  });
+
+  describe('mapStateToProps tests', () => {
+    it('Should pull continents from store', () => {
+
+    });
+  });
+})
