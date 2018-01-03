6f313ea2afed28818a34412aad3737bff8e172ae
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-03 14:20:56 -0700

    Complete AnimalPage testing suite

--- src/containers/AnimalPage/AnimalPage.test.js
+++ src/containers/AnimalPage/AnimalPage.test.js
@@ -7,28 +7,50 @@ import mockProps from '../../data/mockProps';
 
 describe('AnimalPage tests', () => {
   let renderedAnimalPage;
+  let mockRenderAnimalStats;
 
   beforeEach(() => {
     renderedAnimalPage = shallow(
       <AnimalPage animals={mockProps.animals} match={mockProps.match} />
     );
+    mockRenderAnimalStats = renderedAnimalPage.instance().renderAnimalStats();
   });
 
   it('Should match the snapshot', () => {
     expect(renderedAnimalPage).toMatchSnapshot();
   });
 
-  it('cleanAnimalData should return an object', () => {});
+  it('cleanAnimalData should return an object', () => {
+    const mockCleanedData = renderedAnimalPage.instance().cleanAnimalData(mockAnimalsArray[0]);
 
-  it('cleanAnimalData should clean data properties', () => {});
+    expect(typeof mockCleanedData).toEqual('object');
+  });
 
-  it('renderAnimalStats should return an object', () => {});
+  it('cleanAnimalData should clean data properties', () => {
+    const mockCleanedData = renderedAnimalPage.instance().cleanAnimalData(mockAnimalsArray[0]);
+    const expected = mockCleanAnimalData;
 
-  it('renderAnimalStats should have an array for longStats and basicStats', () => {});
+    expect(mockCleanedData).toEqual(expected);
+  });
 
-  it('renderAnimalStats should not render empty properties', () => {});
+  it('renderAnimalStats should return an object', () => {
+    expect(typeof mockRenderAnimalStats).toEqual('object');
+  });
 
-  it('longStats should container "The Facts" and "Why I\'m Important"', () => {});
+  it('renderAnimalStats should have an array for longStats and basicStats', () => {
+    expect(typeof mockRenderAnimalStats.basicStats).toEqual('object');
+    expect(typeof mockRenderAnimalStats.longStats).toEqual('object');
+  });
+
+  it('renderAnimalStats should not render empty properties', () => {
+    expect(renderedAnimalPage.state('animalData').population).toBeDefined;
+
+    renderedAnimalPage.state('animalData').population = '';
+    const mockRenderAnimalStats = renderedAnimalPage.instance().renderAnimalStats();
+    const expected = undefined;
+
+    expect(mockRenderAnimalStats.basicStats.population).toEqual(expected);
+  });
 
   describe('mapStateToProps tests', () => {
     it('Should pull animals from the store', () => {
