55708ef3496bfaa0bf71f240df29d71c49b5104c
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-02 17:14:23 -0700

    Mock out ContinentPage constructor and lifecycle methods

--- src/containers/ContinentPage/ContinentPage.js
+++ src/containers/ContinentPage/ContinentPage.js
@@ -3,6 +3,24 @@ import { connect } from 'react-redux';
 import './ContinentPage.css';
 
 export class ContinentPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      continentsData: {},
+      animals: []
+    };
+  }
+
+  componentWillMount() {
+    if (this.props.continents.length) {
+      this.matchContinent(this.props);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.matchContinent(nextProps);
+  }
+
   render() {
     return <div className="ContinentPage">I am an ContinentPage</div>;
   }
