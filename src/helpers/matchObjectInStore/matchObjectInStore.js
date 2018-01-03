5504e84f7c1669c2303307048699607df679b661
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-02 20:06:49 -0700

    Build matchObjectInStore helper function

---
+++ src/helpers/matchObjectInStore/matchObjectInStore.js
@@ -0,0 +1,11 @@
+const matchObjectInStore = (props, type, group) => {
+  const linkedObject = props.match.params[type];
+  const objectData = props[group].find(
+    object => object.name === linkedObject
+  );
+
+  return objectData
+};
+
+export default matchObjectInStore;
+
