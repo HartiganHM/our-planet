9f81e2c26c1e903284626732c4835b721b9a39da
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-03 11:45:22 -0700

    Pass initial AnimalPage test and MSP test

--- src/containers/AnimalPage/AnimalPage.test.js
+++ src/containers/AnimalPage/AnimalPage.test.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import AnimalPage from './AnimalPage';
+import { AnimalPage, mapStateToProps } from './AnimalPage';
+import mockAnimalsArray from '../../data/mockAnimalsArray';
+import mockProps from '../../data/mockProps';
 
 describe('AnimalPage tests', () => {
   it('Should exist', () => {
-    const renderedAnimalPage = shallow(<AnimalPage />);
+    const renderedAnimalPage = shallow(
+      <AnimalPage animals={mockProps.animals} match={mockProps.match} />
+    );
     expect(renderedAnimalPage).toBeDefined();
+  });
+
+  describe('mapStateToProps tests', () => {
+    it('Should pull animals from the store', () => {
+      const mockStore = {
+        animals: mockAnimalsArray
+      };
+      const result = mapStateToProps(mockStore);
+
+      expect(result.animals).toEqual(mockStore.animals);
+    });
   });
 });
