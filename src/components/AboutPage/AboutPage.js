944cd0ed51b7ef22e64dc52285ed933bf593930b
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 20:27:14 -0700

    Build out superficial AboutPage component

---
+++ src/components/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AboutPage.css';
+
+const AboutPage = () => {
+  return(
+    <div className='AboutPage'>
+      I'm an About Page! :)
+    </div>
+  )
+}
+
+export default AboutPage;
