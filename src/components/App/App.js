011ca7120e82a865934b450c9e497028619a2c50
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-04 20:52:35 -0700

    Remove Header component from App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Header from '../../containers/Header/Header';
 import Routes from '../Routes/Routes';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
       <Routes />
     </div>
   );
