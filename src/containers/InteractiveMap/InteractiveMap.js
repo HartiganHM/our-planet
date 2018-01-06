1766963060484bac123ffd17296b3c87d9f022f7
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-06 10:27:35 -0700

    Build image map and render areas; successfully call image map resize

--- src/containers/InteractiveMap/InteractiveMap.js
+++ src/containers/InteractiveMap/InteractiveMap.js
@@ -3,22 +3,62 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 // import * as actions from '../../actions';
 import PropTypes from 'prop-types';
+import imageMap from '../../data/imageMap';
+import ImageMap from 'image-map';
 import './InteractiveMap.css';
 
 export class InteractiveMap extends Component {
+  constructor() {
+    super();
+    this.state = {
+      src: '../../images/interactive-map/world/OP-Interactive-Map.png'
+    };
+  }
+
+  handleMouse = () => {
+    console.log('in');
+  };
+
+  handleRedirect = continentLink => {
+    return continentLink;
+  };
+
   render() {
     const buttons = this.props.continents.map((continent, index) => {
       const continentLink = '/continents/' + continent.name;
-      const continentClass = 'continent-button ' + continent.name;
+      const continentKabob = continent.image.split(' ').join('-');
+      const continentClass = 'continent-button ' + continentKabob;
+      let continentMap = imageMap[continentKabob];
 
       return (
-        <Link className={continentClass} key={index} to={continentLink}>
-          {continent.name}
-        </Link>
+        <area
+          className={continentKabob}
+          shape="poly"
+          coords={continentMap}
+          onClick={() => this.handleRedirect(continentLink)}
+          onMouseOver={() => this.handleMouse()}
+          onMouseOut={() => this.handleMouse()}
+          alt={continentKabob}
+        />
       );
     });
 
-    return <div className="InteractiveMap">{buttons}</div>;
+    ImageMap('img[usemap]');
+
+    return (
+      <div className="InteractiveMap">
+        <img
+          id="world-image"
+          alt="our-planet-world-map"
+          src={require('../../images/interactive-map/world/OP-Interactive-Map.png')}
+          className="world-image"
+          useMap="#image-map"
+        />
+        <map id="image-map" name="image-map">
+          {buttons}
+        </map>
+      </div>
+    );
   }
 }
 
