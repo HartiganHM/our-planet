0462739afaa6efa565a6d8dd181850ff8e33e5b3
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-06 17:16:00 -0700

    Remove unused images

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Header from '../../containers/Header/Header';
-import InteractiveMap from '../../containers/InteractiveMap/InteractiveMap';
+import InteractiveMap from '../InteractiveMap/InteractiveMap';
 import Search from '../../containers/Search/Search';
 import CardContainer from '../../containers/CardContainer/CardContainer';
 import AboutPage from '../AboutPage/AboutPage';
