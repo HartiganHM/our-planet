fdb048d39fa4e02e5f24b358c88061f8e5f8fe6c
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-06 17:10:23 -0700

    Move InteractiveMap to components directory and remove unused variables

--- src/components/InteractiveMap/InteractiveMap.js
+++ src/components/InteractiveMap/InteractiveMap.js
@@ -1,7 +1,5 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import { Link } from 'react-router-dom';
-// import * as actions from '../../actions';
 import './InteractiveMap.css';
 
 export const InteractiveMap = () => {
