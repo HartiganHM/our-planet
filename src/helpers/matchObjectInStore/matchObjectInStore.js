1015c918a500cf933f910c1e4f281c920471b2e2
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-07 11:14:05 -0700

    Fix conditional check for Ancarctica / Arctic routing

--- src/helpers/matchObjectInStore/matchObjectInStore.js
+++ src/helpers/matchObjectInStore/matchObjectInStore.js
@@ -1,8 +1,9 @@
 const matchObjectInStore = (props, type, group) => {
   const linkedObject =
-    props.match.params[type] === 'Antarctica '
+    props.match.params[type] === 'Antarctica Arctic'
       ? 'Antarctica / Arctic'
       : props.match.params[type];
+      console.log(linkedObject);
   const objectData = props[group].find(object => object.name === linkedObject);
 
   return objectData;
