17d862dcc120096aa6d06a45d881586fdd627f24
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-06 19:20:02 -0700

    Remove unneeded code

--- src/components/InteractiveMap/InteractiveMap.js
+++ src/components/InteractiveMap/InteractiveMap.js
@@ -7,7 +7,6 @@ export const InteractiveMap = () => {
     <div className="InteractiveMap">
       <svg
         version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
         x="0px"
         y="0px"
         viewBox="0 0 2371 1207"
