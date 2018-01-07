356e97361fc27a5a5961e432336fbb6c05975817
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-07 15:05:54 -0700

    Complete AboutPage testing suite

---
+++ src/components/AboutPage/AboutPage.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import AboutPage from './AboutPage';
+
+describe('AboutPage tests', () => {
+  it('Should match the snapshot', () => {
+    const renderedAboutPage = shallow(<AboutPage />);
+
+    expect(renderedAboutPage).toMatchSnapshot();
+  })
+})
