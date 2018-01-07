1abbc17e8ed4c8cf67a44077850d3b3f7fc05db2
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-07 12:37:17 -0700

    Apply standard classNames and add placeholder copy

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -11,20 +11,20 @@ const AboutPage = () => {
 
       <div className='purpose'>
         <div className='purpose-image'></div>
-        <div className='purpose-title'>Purpose</div>
-        <div className='purpose-body'></div>
+        <div className='section-title'>Purpose</div>
+        <div className='section-body'>Purposes and stuff</div>
       </div>
 
       <div className='why'>
         <div className='why-image'></div>
-        <div className='why-title'>Why</div>
-        <div className='why-body'></div>
+        <div className='section-title'>Why</div>
+        <div className='section-body'>Why the hell not?</div>
       </div>
 
       <div className='help'>
         <div className='help-image'></div>
-        <div className='help-title'>How You Can Help</div>
-        <div className='help-body'></div>
+        <div className='section-title'>How You Can Help</div>
+        <div className='section-body'>Don't be a boner</div>
       </div>
     </div>
   );
