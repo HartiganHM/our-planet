c3e0953bb7d438b6b6e346d3165f690e9009f564
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-07 15:03:07 -0700

    Complete InteractiveMap test suite

--- src/components/InteractiveMap/InteractiveMap.test.js
+++ src/components/InteractiveMap/InteractiveMap.test.js
@@ -23,22 +23,55 @@ describe('InteractiveMap tests', () => {
   });
 
   it('Should setState with handleMouseHover', () => {
+    const expectedState = {
+      isHovering: true,
+      continent: 'Asia'
+    };
 
+    renderedMap.instance().handleMouseHover('Asia')
+    expect(renderedMap.state()).toEqual(expectedState);
   });
 
   it('Should setState with setContinent', () => {
+    const expectedContinent = 'Europe';
 
+    renderedMap.instance().setContinent('Europe');
+    expect(renderedMap.state('continent')).toEqual(expectedContinent);
   });
 
   it('Should receive an object from toggleHoverState with an opposite boolean', () => {
+    const expectedReturn = { isHovering: true };
+    const mockReturnObject = renderedMap.instance().toggleHoverState(renderedMap.state())
 
+    expect(mockReturnObject).toEqual(expectedReturn);
   });
 
-  it('Should redurn svg path elements with createPath', () => {
+  it('Should setState with the return object from toggleHoverState', () => {
+    const expectedState = {
+      isHovering: true,
+      continent: ''
+    }
 
+    expect(renderedMap.state('isHovering')).toEqual(false);
+    renderedMap.instance().handleMouseHover('');
+
+    expect(renderedMap.state()).toEqual(expectedState);
+  })
+
+  it('Should return svg path elements with createPath', () => {
+    const mockPath = renderedMap.instance().createPath('north-america', 'North America');
+    const expectedTransform = continentsPaths['north-america'].transform;
+    const expectedPath = continentsPaths['north-america'].path;
+
+    expect(mockPath.props.children.props.transform).toEqual(expectedTransform);
+    expect(mockPath.props.children.props.children.props.children.props.d).toEqual(expectedPath);
   });
 
-  it('Should not render a Link component with createPath when passed grid', () => {
+  it('Should return different render when createPath is passed grid', () => {
+    const mockPath = renderedMap.instance().createPath('grid');
+    const renderedGrid = renderedMap.find('.grid').prop('d');
+    const expectedPath = mockPath.props.children.props.children.props.d;
 
+    expect(renderedGrid).toEqual(expectedPath);
   });
 });
