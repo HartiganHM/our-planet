dd2ee0056f3d8c790748480fa697edd962a30c85
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-07 12:39:55 -0700

    Extend ipsum placeholder text

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -12,19 +12,19 @@ const AboutPage = () => {
       <div className='purpose'>
         <div className='purpose-image'></div>
         <div className='section-title'>Purpose</div>
-        <div className='section-body'>Purposes and stuff</div>
+        <div className='section-body'>Under the bed ooh, are those your $250 dollar sandals? lemme use that as my litter box, or weigh eight pounds but take up a full-size bed. Climb a tree, wait for a fireman jump to fireman then scratch his face meow. Munch on tasty moths flex claws on the human's belly and purr like a lawnmower woops poop hanging from butt must get rid run run around house drag poop on floor maybe it comes off woops left brown marks on floor human slave clean lick butt now flee in terror at cucumber discovered on floor or if it smells like fish eat as much as you wish. </div>
       </div>
 
       <div className='why'>
         <div className='why-image'></div>
         <div className='section-title'>Why</div>
-        <div className='section-body'>Why the hell not?</div>
+        <div className='section-body'>Ask for petting. Pee in human's bed until he cleans the litter box eat plants, meow, and throw up because i ate plants. Hide head under blanket so no one can see fall asleep upside-down lick human with sandpaper tongue. Howl uncontrollably for no reason Gate keepers of hell you call this cat food stare at ceiling behind the couch. Meow scratch the furniture. This cat happen now, it was too purr-fect!!! get video posted to internet for chasing red dot sleep and need to chase tail purr for no reason for knock dish off table head butt cant eat out of my own dish.</div>
       </div>
 
       <div className='help'>
         <div className='help-image'></div>
         <div className='section-title'>How You Can Help</div>
-        <div className='section-body'>Don't be a boner</div>
+        <div className='section-body'>Make muffins eat grass, throw it back up jump on human and sleep on her all night long be long in the bed, purr in the morning and then give a bite to every human around for not waking up request food, purr loud scratch the walls, the floor, the windows, the humans. Throw down all the stuff in the kitchen cough furball so meow go back to sleep owner brings food and water tries to pet on head, so scratch get sprayed by water because bad cat sniff catnip and act crazy sniff catnip and act crazy dead stare with ears cocked.</div>
       </div>
     </div>
   );
