85618a290ddec1aa2ee14b0535c4dd28f4fa72cd
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-08 16:29:35 -0700

    Mock out anticipated tests for setStickyProperties

---
+++ src/helpers/setStickyProperties/setStickyProperties.test.js
@@ -0,0 +1,19 @@
+import setStickyProperties from './setStickyProperties';
+
+describe('setStickyProperties tests', () => {
+  it('Should be a function', () => {
+    expect(setStickyProperties).toBeAFunction;
+  });
+
+  it('Should setInitialHeight of elements', () => {
+
+  });
+
+  it('Should gather all sticky elements', () => {
+
+  });
+
+  it('Should add an event listener to the document', () => {
+
+  })
+});
