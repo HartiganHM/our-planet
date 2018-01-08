6382484b7a9e2f482a1884e98bc2421209a0716a
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-07 22:06:02 -0700

    Fix spacing and typo, export component

--- src/components/Sticky/Sticky.js
+++ src/components/Sticky/Sticky.js
@@ -5,8 +5,11 @@ import './Sticky.css';
 class Sticky extends Component {
   componentDidMount() {
     const setInitialHeights = elements => {
-      [].forEach.call(elements, (sticky) => {
-        sticky.setAttribute('data-sticky-initial', sticky.getBoundingClientRect().top);
+      [].forEach.call(elements, sticky => {
+        sticky.setAttribute(
+          'data-sticky-initial',
+          sticky.getBoundingClientRect().top
+        );
       });
     };
 
@@ -15,20 +18,27 @@ class Sticky extends Component {
 
     document.addEventListener('scroll', () => {
       const top = document.documentElement.scrollTop || document.body.scrollTop;
-      const bottom = document.documentElement.scrollHeight || document.body.scrollHeight;
-    })
+      const bottom =
+        document.documentElement.scrollHeight || document.body.scrollHeight;
 
-    [].forEach.call(stickies, (sticky) => {
-      const stickyInitial = parseInt(sticky.getAttribute('data-sticky-initial'), 10);
-      const stickyEnter = parseInt(sticky.getAttribute('data-sticky-enter'), 10) || stickyInitial;
-      const stickyExit = parseInt(sticky.getAttribute('data-sticky-exit'), 10) || bottom;
+      [].forEach.call(stickies, sticky => {
+        const stickyInitial = parseInt(
+          sticky.getAttribute('data-sticky-initial'),
+          10
+        );
+        const stickyEnter =
+          parseInt(sticky.getAttribute('data-sticky-enter'), 10) ||
+          stickyInitial;
+        const stickyExit =
+          parseInt(sticky.getAttribute('data-sticky-exit'), 10) || bottom;
 
-      if (top >= stickyEnter && top <= stickyExit) {
-        sticky.classList.add('sticky');
-      } else {
-        sticky.classList.remove('sticky');
-      }
-    })
+        if (top >= stickyEnter && top <= stickyExit) {
+          sticky.classList.add('sticky');
+        } else {
+          sticky.classList.remove('sticky');
+        }
+      });
+    });
   }
 
   render() {
@@ -45,6 +55,8 @@ class Sticky extends Component {
     );
   }
 }
+
+export default Sticky;
 
 Sticky.propTypes = {
   className: PropTypes.string,
