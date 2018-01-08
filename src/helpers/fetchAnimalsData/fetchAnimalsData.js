c9abcd81fb9b00aabe8fbb4ddb3f40d0cf098ab4
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-08 08:26:31 -0700

    Call sortAnimalsAlphabetically in fetchAnimalsData

--- src/helpers/fetchAnimalsData/fetchAnimalsData.js
+++ src/helpers/fetchAnimalsData/fetchAnimalsData.js
@@ -1,11 +1,14 @@
 import addPropertiesToAnimal from '../addPropertiesToAnimal/addPropertiesToAnimal';
+import sortAnimalsAlphabetically from '../sortAnimalsAlphabetically/sortAnimalsAlphabetically';
 
 const fetchAnimalsData = async () => {
   try {
     const fetchedData = await fetch('https://our-planet-be.herokuapp.com/api/v1/animals');
     const jsonData = await fetchedData.json();
+    const newAnimals = addPropertiesToAnimal(jsonData);
+    const sortedAnimals = sortAnimalsAlphabetically(newAnimals)
 
-    return addPropertiesToAnimal(jsonData);
+    return sortedAnimals;
 
   } catch (error) {
     return new Error(`Error fetching animals data: ${error}`);
