4016d21ae57e725332740fc18a09e709d035f50e
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-07 21:50:03 -0700

    Import styling into Sticky component

--- src/components/Sticky/Sticky.js
+++ src/components/Sticky/Sticky.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import './Sticky.css';
+import './Sticky.css';
 
 class Sticky extends Component {
   componentDidMount() {
