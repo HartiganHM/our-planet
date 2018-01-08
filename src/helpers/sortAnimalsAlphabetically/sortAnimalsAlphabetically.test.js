5f10024fe8293182f0222b1334653db1adb07f98
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-08 08:50:52 -0700

    Complete sortAnimalsAlphabetically test suite

--- src/helpers/sortAnimalsAlphabetically/sortAnimalsAlphabetically.test.js
+++ src/helpers/sortAnimalsAlphabetically/sortAnimalsAlphabetically.test.js
@@ -1,16 +1,22 @@
-import sortAnimalsAlphabeticall from './sortAnimalsAlphabetically';
+import sortAnimalsAlphabetically from './sortAnimalsAlphabetically';
 import mockAnimalsArray from '../../data/mockAnimalsArray';
 
 describe('sortAnimalsAlphabetically tests', () => {
   it('Should be a function', () => {
-
+    expect(sortAnimalsAlphabetically).toBeAFunction;
   });
 
   it('Should return an array', () => {
+    const mockSortedAnimals = sortAnimalsAlphabetically(mockAnimalsArray);
 
+    expect(typeof mockSortedAnimals).toEqual('object');
   });
 
   it('Should sort animals by name alphabetically', () => {
-    
-  })
+    const jumbledArray = [{ name: 'Whale' }, { name: 'Arctic Wolf' }];
+    const expected = [{ name: 'Arctic Wolf' }, { name: 'Whale' }];
+    const mockSortedAnimals = sortAnimalsAlphabetically(jumbledArray);
+
+    expect(mockSortedAnimals).toEqual(expected);
+  });
 });
