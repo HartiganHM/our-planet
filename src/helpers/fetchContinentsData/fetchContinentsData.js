c3716ae4e83e189bccff3a31b742cd658dd2b825
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-07 20:51:34 -0700

    Adjust link for fetchContinentsData to heroku app

--- src/helpers/fetchContinentsData/fetchContinentsData.js
+++ src/helpers/fetchContinentsData/fetchContinentsData.js
@@ -2,7 +2,7 @@ import addPropertiesToContinent from '../addPropertiesToContinent/addPropertiesT
 
 const fetchContinentsData = async () => {
   try {
-    const fetchedData = await fetch('http://localhost:3001/api/v1/continents');
+    const fetchedData = await fetch('https://our-planet-be.herokuapp.com/api/v1/continents');
     const jsonData = await fetchedData.json();
     return addPropertiesToContinent(jsonData);
 
