2626a85a4b681995bbc470425b55a4396592bfb4
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-08 08:46:08 -0700

    Mock out anticipated sortAnimalsAlphabetically tests

---
+++ src/helpers/sortAnimalsAlphabetically/sortAnimalsAlphabetically.test.js
@@ -0,0 +1,16 @@
+import sortAnimalsAlphabeticall from './sortAnimalsAlphabetically';
+import mockAnimalsArray from '../../data/mockAnimalsArray';
+
+describe('sortAnimalsAlphabetically tests', () => {
+  it('Should be a function', () => {
+
+  });
+
+  it('Should return an array', () => {
+
+  });
+
+  it('Should sort animals by name alphabetically', () => {
+    
+  })
+});
