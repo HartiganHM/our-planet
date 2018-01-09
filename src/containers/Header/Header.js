fc8fc08269be374eb12e5267fb45d099956f4276
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-09 12:24:21 -0700

    Add new gif for landing page

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import * as actions from '../../actions';
 import PropTypes from 'prop-types';
 import './Header.css';
