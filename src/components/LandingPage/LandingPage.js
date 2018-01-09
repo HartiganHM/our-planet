a85ff9c9fb1e432f346f8c135e66d018aefab58a
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-08 22:37:45 -0700

    Remove placeholder text

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,8 +6,6 @@ const LandingPage = () => {
     <div className="LandingPage">
       <div className='inner'>
         <div className='content'>
-          <h1>Our Planet</h1>
-          <a className='button' href='#'>Get Started</a>
         </div>
       </div>
     </div>
