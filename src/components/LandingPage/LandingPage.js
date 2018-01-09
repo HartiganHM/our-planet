2b58bf12d407f92cb929c65f30eadbac88fdb27c
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-08 17:47:31 -0700

    Build out mock component for rendering

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// import './LandingPage.css';
+
+const LandingPage = () => {
+  return <div className="LandingPage">I am a Landing Page!</div>;
+};
+
+export default LandingPage;
