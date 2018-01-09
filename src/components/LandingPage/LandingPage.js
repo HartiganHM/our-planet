37c8bf925a9d0971b3c424591932b185162afc3d
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-08 20:18:19 -0700

    Build out mock LandingPage jsx

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,7 +2,16 @@ import React from 'react';
 import './LandingPage.css';
 
 const LandingPage = () => {
-  return <div className="LandingPage">I am a Landing Page!</div>;
+  return (
+    <div className="LandingPage">
+      <div className='inner'>
+        <div className='content'>
+          <h1>Our Planet</h1>
+          <a className='button' href='#'>Get Started</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
