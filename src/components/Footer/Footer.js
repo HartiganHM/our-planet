bdf6497a24a6a403878193959c3ffeffc97c302f
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-09 21:23:58 -0700

    Build out social links section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,12 @@ const Footer = () => {
   return (
     <div className='Footer'>
       <div className='wrapper'>
-        <span>{copyContent.footer}</span>
+        <span className="footer-copy">{copyContent.footer}</span>
+        <div className="footer-links">
+          <a href="https://github.com/HartiganHM/our-planet" className="github-fe">GitHub Front End Repository</a>
+          <a href="https://github.com/HartiganHM/our-planet-be" className="github-be">GitHub Back End Repository</a>
+          <a href="mailto:hartigan.hm@gmail.com" className="contact">Contact Hugh</a>
+        </div>
       </div>
     </div>
   )
