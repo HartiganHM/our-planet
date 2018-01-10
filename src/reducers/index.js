adbc13d2584470b9522eb17755dce80473cf65b4
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-10 12:51:29 -0700

    Build filter directory and reducers files; import into rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import animalsReducers from './animals/animalsReducers';
 import continentsReducers from './continents/continentsReducers';
+import filterReducers from './filters/filterReducers';
 
 
 const rootReducer = combineReducers({
