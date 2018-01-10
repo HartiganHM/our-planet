425cba79e841cdba8892e49aabf727aea589839b
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-09 22:03:06 -0700

    Rearrange footer icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,8 +9,8 @@ const Footer = () => {
         <span className="footer-copy">{copyContent.footer}</span>
         <div className="footer-links">
           <a href="https://github.com/HartiganHM/our-planet" className="link"><div className="icon github" /><span>FE</span></a>
-          <a href="https://github.com/HartiganHM/our-planet-be" className="link"><div className="icon github" /><span>BE</span></a>
           <a href="mailto:hartigan.hm@gmail.com" className="link"><div className="icon contact" /><span>Contact</span></a>
+          <a href="https://github.com/HartiganHM/our-planet-be" className="link"><div className="icon github" /><span>BE</span></a>
         </div>
       </div>
     </div>
