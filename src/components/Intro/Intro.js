0f0c659df523c165ffa4375356cea977d4145952
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-09 18:28:46 -0700

    Render div for logo placement in Intro component

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -4,10 +4,11 @@ import './Intro.css';
 
 const Intro = () => {
   return (
-    <div className='Intro'>
-      <div className='intro-body'>{copyContent.intro}</div>
+    <div className="Intro">
+      <div className="intro-body">{copyContent.intro}</div>
+      <div className="intro-logo" />
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
