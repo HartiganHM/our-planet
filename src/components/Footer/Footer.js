c209138fa451de4e24820ced78ecb49997e976d6
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-09 22:02:11 -0700

    Adjust Footer link classes and content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,9 +8,9 @@ const Footer = () => {
       <div className='wrapper'>
         <span className="footer-copy">{copyContent.footer}</span>
         <div className="footer-links">
-          <a href="https://github.com/HartiganHM/our-planet" className="link"><div className="github-icon" /><span>Front End</span></a>
-          <a href="https://github.com/HartiganHM/our-planet-be" className="link"><div className="github-icon" /><span>Back End</span></a>
-          <a href="mailto:hartigan.hm@gmail.com" className="link"><div className="contact-icon" /><span>Contact</span></a>
+          <a href="https://github.com/HartiganHM/our-planet" className="link"><div className="icon github" /><span>FE</span></a>
+          <a href="https://github.com/HartiganHM/our-planet-be" className="link"><div className="icon github" /><span>BE</span></a>
+          <a href="mailto:hartigan.hm@gmail.com" className="link"><div className="icon contact" /><span>Contact</span></a>
         </div>
       </div>
     </div>
