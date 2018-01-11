aa2ea3c860893205ff33e6d11f2a0d73ee8d3afc
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-10 18:59:29 -0700

    Complete FilterButtons test suite

--- src/containers/FilterButtons/FilterButtons.test.js
+++ src/containers/FilterButtons/FilterButtons.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { FilterButtons, mapStateToProps } from './FilterButtons';
+import {
+  FilterButtons,
+  mapStateToProps,
+  mapDispatchToProps
+} from './FilterButtons';
 
 describe('FilterButton tests', () => {
   it('Should match the snapshot', () => {
@@ -9,5 +13,28 @@ describe('FilterButton tests', () => {
     const renderedFilterButtons = shallow(
       <FilterButtons filter={mockFilter} switchFilter={mockSwitchFilter} />
     );
+
+    expect(renderedFilterButtons).toMatchSnapshot();
+  });
+
+  describe('mapStateToProps tests', () => {
+    it('Should pull filter from the store', () => {
+      const mockStore = {
+        filter: 'status'
+      };
+      const result = mapStateToProps(mockStore);
+
+      expect(result.filter).toEqual(mockStore.filter);
+    });
+  });
+
+  describe('mapDispatchToProps tests', () => {
+    it('Should call dispatch when switchFilter is called', () => {
+      const mockDispatch = jest.fn();
+      const result = mapDispatchToProps(mockDispatch);
+
+      result.switchFilter('habitat');
+      expect(mockDispatch).toHaveBeenCalled();
+    });
   });
 });
