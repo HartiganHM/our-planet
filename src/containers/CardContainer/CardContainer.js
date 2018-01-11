c441b13c465f1af485006923ae33e42b92cf0f8e
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-10 19:03:46 -0700

    Move card to proper components directory and adjust file paths

--- src/containers/CardContainer/CardContainer.js
+++ src/containers/CardContainer/CardContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from '../Card/Card';
+import Card from '../../components/Card/Card';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import './CardContainer.css';
