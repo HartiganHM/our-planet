3fb4e845c2fbb44f7d37c24dac56d66911e72588
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-10 18:48:28 -0700

    Complete changeFilter action tests

--- src/actions/tests/actionTests.test.js
+++ src/actions/tests/actionTests.test.js
@@ -30,6 +30,18 @@ describe('Actions tests', () => {
       filteredAnimals
     };
 
-    expect(actions.searchAnimals('fox', filteredAnimals, 'name')).toEqual(expected);
+    expect(actions.searchAnimals('fox', filteredAnimals, 'name')).toEqual(
+      expected
+    );
+  });
+
+  it('changeFilter should have a type of CHANGE_FILTER', () => {
+    const filterValue = 'status';
+    const expected = {
+      type: 'CHANGE_FILTER',
+      filterValue
+    };
+
+    expect(actions.changeFilter(filterValue)).toEqual(expected);
   });
 });
