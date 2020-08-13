3cd99d4d80eddd9bbe974ba32674e5fc1f8e74e9
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-08-13 09:41:42 -0600

    Updates

---
+++ precache-manifest.2979db1896f1232ed83e1e398361fdd0.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba807624664ba46499ae5bb1bdb9f94",
+    "url": "/our-planet/index.html"
+  },
+  {
+    "revision": "b83421654c181dc58f88",
+    "url": "/our-planet/static/css/main.93c36b9e.chunk.css"
+  },
+  {
+    "revision": "992034e3c826cde38989",
+    "url": "/our-planet/static/js/2.c894fe75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/our-planet/static/js/2.c894fe75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b83421654c181dc58f88",
+    "url": "/our-planet/static/js/main.74d87429.chunk.js"
+  },
+  {
+    "revision": "5dd7337cbacb54f142a5",
+    "url": "/our-planet/static/js/runtime-main.dba4a0bf.js"
+  },
+  {
+    "revision": "2496acff0bc6974eb7efe10a1d43d2a0",
+    "url": "/our-planet/static/media/OP-Logo-Light-Blue.2496acff.png"
+  },
+  {
+    "revision": "610a297c136f1e6c4f7c97ab3056d3d6",
+    "url": "/our-planet/static/media/Our-Planet-Light-Logo.610a297c.png"
+  },
+  {
+    "revision": "40ffcb1a9f7fc1d5a36961b4a6a747a2",
+    "url": "/our-planet/static/media/Our-Planet-Wordmark-Large.40ffcb1a.png"
+  },
+  {
+    "revision": "8cff0d5f402ef5e08a9aea526462bfca",
+    "url": "/our-planet/static/media/Our-Planet-Wordmark-Small.8cff0d5f.png"
+  },
+  {
+    "revision": "a595dfd8b02041c6d94a3cd0a381b167",
+    "url": "/our-planet/static/media/bear-about.a595dfd8.jpg"
+  },
+  {
+    "revision": "92c8bc5ef91e080d6973134ef7181bb3",
+    "url": "/our-planet/static/media/contact-icon-blue.92c8bc5e.svg"
+  },
+  {
+    "revision": "d07111d10d1d204e0bb8ff601e6c6c62",
+    "url": "/our-planet/static/media/contact-icon-snow.d07111d1.svg"
+  },
+  {
+    "revision": "fe381cf1ce78d9d8d9329592814ee7d9",
+    "url": "/our-planet/static/media/elephant-about.fe381cf1.jpg"
+  },
+  {
+    "revision": "e4708a9e12f76d390ee7a647fa7b9590",
+    "url": "/our-planet/static/media/frog-about.e4708a9e.jpg"
+  },
+  {
+    "revision": "78a29794b2c0308ef007bced81860a12",
+    "url": "/our-planet/static/media/gh-icon-blue.78a29794.svg"
+  },
+  {
+    "revision": "47361d5c9d8d1b8ed2fe8ac560754cb9",
+    "url": "/our-planet/static/media/gh-icon-snow.47361d5c.svg"
+  },
+  {
+    "revision": "ed202d09e834ef8e38d94a0360b0015c",
+    "url": "/our-planet/static/media/landing-seascape.ed202d09.jpg"
+  },
+  {
+    "revision": "2a68e88611cccf8b5063b143cbc79ff5",
+    "url": "/our-planet/static/media/op-globe-animation.2a68e886.gif"
+  }
+]);
--- src/helpers/addPropertiesToContinent/addPropertiesToContinent.js
+++
@@ -1,9 +0,0 @@
-const addPropertiesToContinent = continentsArray => {
-  return continentsArray.map(continent => {
-    const image = continent.image_url;
-
-    return { ...continent, image };
-  });
-};
-
-export default addPropertiesToContinent;
