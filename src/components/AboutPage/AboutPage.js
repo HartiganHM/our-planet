c5a7d60e90c7db3d8fcf6af62c056030568efbef
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-08-15 15:43:58 -0600

    refactor: AboutPage updates

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import copyContent from '../../data/copyContent';
+
 import './AboutPage.scss';
 
 const AboutPage = () => {
-  window.scrollTo(0, 0);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const {
+    helpDescription,
+    helpTitle,
+    purposeDescription,
+    purposeTitle,
+    whyDescription,
+    whyTitle,
+  } = copyContent.about;
+
   return (
     <div className="AboutPage buffer">
       <div className="purpose">
         <div className="parallax purpose-image" />
-        <div className="section-title">Purpose</div>
-        <div className="section-body">{copyContent.purpose}</div>
+        <div className="section-title">{purposeTitle}</div>
+        <div className="section-body">{purposeDescription}</div>
       </div>
 
       <div className="why">
         <div className="parallax why-image" />
-        <div className="section-title">Why</div>
-        <div className="section-body">{copyContent.why}</div>
+        <div className="section-title">{whyTitle}</div>
+        <div className="section-body">{whyDescription}</div>
       </div>
 
       <div className="help">
         <div className="parallax help-image" />
-        <div className="section-title">How You Can Help</div>
-        <div className="section-body">{copyContent.help}</div>
+        <div className="section-title">{helpTitle}</div>
+        <div className="section-body">{helpDescription}</div>
       </div>
     </div>
   );
