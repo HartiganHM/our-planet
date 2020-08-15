5170692861959b70e3bc86aad6a5b5faf0136e0a
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-08-15 15:47:43 -0600

    refactor: App updates

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import Routes from '../Routes/Routes';
 import './App.scss';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Routes />
-    </div>
-  );
-};
+const App = () => (
+  <div className="app">
+    <Routes />
+  </div>
+);
 
 export default App;
