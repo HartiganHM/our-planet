fb933b359087590deb90a27d3a19f15e6fd0a12b
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-04-10 09:26:06 -0600

    refactor: Update InteractiveMap to use hooks

--- src/components/InteractiveMap/InteractiveMap.js
+++ src/components/InteractiveMap/InteractiveMap.js
@@ -1,32 +1,18 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import continentsPaths from '../../data/continentsPaths';
 import './InteractiveMap.scss';
 
-class InteractiveMap extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isHovering: false,
-      continent: '',
-    };
-  }
+const InteractiveMap = () => {
+  const [isHovering, updateHovering] = useState(false);
+  const [continent, updateContinent] = useState('');
 
-  handleMouseHover = continent => {
-    this.setState(this.toggleHoverState, this.setContinent(continent));
+  const handleMouseHover = continent => {
+    updateHovering(!isHovering);
+    updateContinent(continent);
   };
 
-  setContinent = continent => {
-    this.setState({ continent });
-  };
-
-  toggleHoverState(state) {
-    return {
-      isHovering: !state.isHovering,
-    };
-  }
-
-  createPath = (className, name) => {
+  const createPath = (className, name) => {
     const continentLink =
       name === 'The Oceans'
         ? '/our-planet/continents/Oceans'
@@ -48,8 +34,8 @@ class InteractiveMap extends Component {
           <g transform={continentsPaths[className].transform}>
             <Link to={continentLink}>
               <path
-                onMouseEnter={() => this.handleMouseHover(displayName)}
-                onMouseLeave={() => this.handleMouseHover('')}
+                onMouseEnter={() => handleMouseHover(displayName)}
+                onMouseLeave={() => handleMouseHover('')}
                 className={className === 'oceans' ? className : 'continent'}
                 d={continentsPaths[className].path}
               />
@@ -60,51 +46,44 @@ class InteractiveMap extends Component {
     }
   };
 
-  render() {
-    const hoverBox = (
-      <div className="hover-box">
-        <div className="hover-title">What animals are endangered near you?</div>
-        <div className="hover-body">
-          <div>
-            Use our map to search the globe and view endangered animals from...
-          </div>
-          <div className="hover-continent">
-            {this.state.isHovering ? this.state.continent : ' - '}
-          </div>
+  const hoverBox = (
+    <div className="hover-box">
+      <div className="hover-title">What animals are endangered near you?</div>
+      <div className="hover-body">
+        <div>
+          Use our map to search the globe and view endangered animals from...
         </div>
+        <div className="hover-continent">{isHovering ? continent : ' - '}</div>
       </div>
-    );
+    </div>
+  );
 
-    const oceans = this.createPath('oceans', 'The Oceans');
-    const grid = this.createPath('grid');
-    const northAmerica = this.createPath('north-america', 'North America');
-    const southAmerica = this.createPath('south-america', 'South America');
-    const africa = this.createPath('africa', 'Africa');
-    const australia = this.createPath('australia', 'Australia');
-    const europe = this.createPath('europe', 'Europe');
-    const asia = this.createPath('asia', 'Asia');
-    const antarcticaArctic = this.createPath(
-      'antarctica-arctic',
-      'Antarctica Arctic'
-    );
+  const oceans = createPath('oceans', 'The Oceans');
+  const grid = createPath('grid');
+  const northAmerica = createPath('north-america', 'North America');
+  const southAmerica = createPath('south-america', 'South America');
+  const africa = createPath('africa', 'Africa');
+  const australia = createPath('australia', 'Australia');
+  const europe = createPath('europe', 'Europe');
+  const asia = createPath('asia', 'Asia');
+  const antarcticaArctic = createPath('antarctica-arctic', 'Antarctica Arctic');
 
-    return (
-      <div className="InteractiveMap">
-        {hoverBox}
-        <svg version="1.1" x="0px" y="0px" viewBox="0 0 2371 1207">
-          {oceans}
-          {grid}
-          {northAmerica}
-          {southAmerica}
-          {africa}
-          {australia}
-          {europe}
-          {asia}
-          {antarcticaArctic}
-        </svg>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="InteractiveMap">
+      {hoverBox}
+      <svg version="1.1" x="0px" y="0px" viewBox="0 0 2371 1207">
+        {oceans}
+        {grid}
+        {northAmerica}
+        {southAmerica}
+        {africa}
+        {australia}
+        {europe}
+        {asia}
+        {antarcticaArctic}
+      </svg>
+    </div>
+  );
+};
 
 export default InteractiveMap;
