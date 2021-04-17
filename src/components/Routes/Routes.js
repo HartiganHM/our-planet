c70edd24736af537dfdcf5c49ef817a7c9f288db
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-04-17 13:33:34 -0600

    Add Router to Routes with basename

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,44 +1,42 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import LandingPage from '../LandingPage/LandingPage';
-import Sticky from '../Sticky/Sticky';
-import Intro from '../Intro/Intro';
-import InteractiveMap from '../InteractiveMap/InteractiveMap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import {
+  AboutPage,
+  InteractiveMap,
+  Intro,
+  LandingPage,
+  Sticky,
+} from '../index';
+
 import Search from '../../containers/Search/Search';
 import CardContainer from '../../containers/CardContainer/CardContainer';
-import AboutPage from '../AboutPage/AboutPage';
 import AnimalPage from '../../containers/AnimalPage/AnimalPage';
 import ContinentPage from '../../containers/ContinentPage/ContinentPage';
+
 import Footer from '../Footer/Footer';
 
-const Routes = () => {
-  return (
-    <div>
-      <Route exact path="/our-planet/" component={LandingPage} />
-      <Route path="/our-planet/" component={Sticky} />
-      <Route
-        exact
-        path="/our-planet/"
-        render={() => {
-          return (
-            <div className="buffer">
-              <Intro />
-              <InteractiveMap />
-              <Search />
-              <CardContainer />
-            </div>
-          );
-        }}
-      />
-      <Route exact path="/our-planet/about" component={AboutPage} />
-      <Route path="/our-planet/animals/:animal" component={AnimalPage} />
-      <Route
-        path="/our-planet/continents/:continent"
-        component={ContinentPage}
-      />
-      <Route path="/our-planet/" component={Footer} />
-    </div>
-  );
-};
+const Routes = () => (
+  <Router basename="/our-planet">
+    <Route exact path="/" component={LandingPage} />
+    <Route path="/" component={Sticky} />
+    <Route
+      exact
+      path="/"
+      render={() => (
+        <div className="buffer">
+          <Intro />
+          <InteractiveMap />
+          <Search />
+          <CardContainer />
+        </div>
+      )}
+    />
+    <Route exact path="/about" component={AboutPage} />
+    <Route path="/animals/:animal" component={AnimalPage} />
+    <Route path="/continents/:continent" component={ContinentPage} />
+    <Route path="/" component={Footer} />
+  </Router>
+);
 
 export default Routes;
