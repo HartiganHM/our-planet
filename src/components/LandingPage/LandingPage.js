3d5dbe12002912d452b00945a9184370f38ecc0c
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-04-17 13:19:18 -0600

    Refactor LandingPage component

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,14 +1,13 @@
 import React from 'react';
+
 import './LandingPage.scss';
 
-const LandingPage = () => {
-  return (
-    <div className="LandingPage">
-      <div className="inner">
-        <div className="content" />
-      </div>
+const LandingPage = () => (
+  <div className="LandingPage">
+    <div className="inner">
+      <div className="content" />
     </div>
-  );
-};
+  </div>
+);
 
 export default LandingPage;
