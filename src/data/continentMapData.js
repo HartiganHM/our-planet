6246ab33197767ac9f3cb5ba944f5e67ef65d780
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-04-17 13:15:32 -0600

    Build continentMapData

---
+++ src/data/continentMapData.js
@@ -0,0 +1,48 @@
+const continentMapData = {
+  oceans: {
+    className: 'oceans',
+    link: '/our-planet/continents/Oceans',
+    name: 'The Oceans',
+  },
+  grid: {
+    className: 'grid',
+  },
+  antarcticaArctic: {
+    className: 'antarctica-arctic',
+    displayName: 'Antarctica / Arctic',
+    link: '/our-planet/continents/',
+    name: 'Antarctica Arctic',
+  },
+  africa: {
+    className: 'africa',
+    link: '/our-planet/continents/',
+    name: 'Africa',
+  },
+  asia: {
+    className: 'asia',
+    link: '/our-planet/continents/',
+    name: 'Asia',
+  },
+  australia: {
+    className: 'australia',
+    link: '/our-planet/continents/',
+    name: 'Australia',
+  },
+  europe: {
+    className: 'europe',
+    link: '/our-planet/continents/',
+    name: 'Europe',
+  },
+  northAmerica: {
+    className: 'north-america',
+    link: '/our-planet/continents/north-america',
+    name: 'North America',
+  },
+  southAmerica: {
+    className: 'south-america',
+    link: '/our-planet/continents/',
+    name: 'South America',
+  },
+};
+
+export default continentMapData;
---
+++ src/data/index.js
@@ -0,0 +1,17 @@
+import continentMapData from './continentMapData';
+import continentsPaths from './continentsPaths';
+import copyContent from './copyContent';
+import mockAnimalsArray from './mockAnimalsArray';
+import mockCleanAnimalData from './mockCleanAnimalData';
+import mockContinentsArray from './mockContinentsArray';
+import mockProps from './mockProps';
+
+export {
+  continentMapData,
+  continentsPaths,
+  copyContent,
+  mockAnimalsArray,
+  mockCleanAnimalData,
+  mockContinentsArray,
+  mockProps,
+};
