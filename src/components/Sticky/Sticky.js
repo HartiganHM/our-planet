59ca7d9f98f387d291f66d355b506cf34295d676
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-04-17 13:52:53 -0600

    Refactor Sticket component

--- src/components/Sticky/Sticky.js
+++ src/components/Sticky/Sticky.js
@@ -1,47 +1,44 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import setStickyProperties from '../../helpers/setStickyProperties/setStickyProperties';
+
 import Header from '../../containers/Header/Header';
+import { setStickyProperties } from '../../helpers';
+
 import './Sticky.scss';
 
-class Sticky extends Component {
-  componentDidMount() {
+const Sticky = ({ className, location: { pathname } }) => {
+  useEffect(() => {
     setStickyProperties();
-  }
+  }, []);
 
-  render() {
-    const location = this.props.location.pathname;
-    const { className, exit } = this.props;
-    let enter;
-
+  const calculateEnterHeight = () => {
     if (
-      location === '/about' ||
-      location.includes('animals') ||
-      location.includes('continents')
+      pathname === '/about' ||
+      pathname.includes('animals') ||
+      pathname.includes('continents')
     ) {
-      enter = '1';
-    } else {
-      enter = (window.outerHeight + window.innerHeight) / 2;
+      return 1;
     }
 
-    return (
-      <div
-        className={`Sticky ${className}`}
-        data-sticky
-        data-sticky-enter={enter}
-        data-stick-exit={exit}
-      >
-        <Header location={location} />
-      </div>
-    );
-  }
-}
+    return (window.outerHeight + window.innerHeight) / 2;
+  };
+
+  const enter = calculateEnterHeight();
+
+  return (
+    <div
+      className={`Sticky ${className}`}
+      data-sticky
+      data-sticky-enter={enter}
+    >
+      <Header location={pathname} />
+    </div>
+  );
+};
 
 export default Sticky;
 
 Sticky.propTypes = {
   className: PropTypes.string,
-  enter: PropTypes.string,
-  exit: PropTypes.string,
   location: PropTypes.object,
 };
