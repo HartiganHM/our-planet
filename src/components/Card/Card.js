06058751b16dad59b0d86f693ca8aecde25942a7
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-04-17 13:34:29 -0600

    Refactor Card component for automatic return

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { object } from 'prop-types';
+import { shape, string } from 'prop-types';
 
 import './Card.scss';
 
-const Card = props => {
-  const { name, image } = props.animalData;
-
-  return (
-    <Link className="card" to={`/our-planet/animals/${name}`}>
-      <img
-        className="card__animal-image"
-        src={image}
-        alt={`${name}-endangered-species`}
-      />
-      <span className="card__animal-name">{name}</span>
-    </Link>
-  );
-};
+const Card = ({ animalData: { name, image } }) => (
+  <Link className="card" to={`/animals/${name}`}>
+    <img
+      className="card__animal-image"
+      src={image}
+      alt={`${name}-endangered-species`}
+    />
+    <span className="card__animal-name">{name}</span>
+  </Link>
+);
 
 export default Card;
 
 Card.propTypes = {
-  animalData: object,
+  animalData: shape({
+    name: string,
+    image: string,
+  }),
 };
