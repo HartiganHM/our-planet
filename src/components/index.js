45f8e003ea892bf580f70c6093f7c797b1ecc436
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-04-17 13:34:56 -0600

    Add index for /components

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import AboutPage from './AboutPage/AboutPage';
+import App from './App/App';
+import Card from './Card/Card';
+import Footer from './Footer/Footer';
+import InteractiveMap from './InteractiveMap/InteractiveMap';
+import Intro from './Intro/Intro';
+import LandingPage from './LandingPage/LandingPage';
+import Routes from './Routes/Routes';
+import Sticky from './Sticky/Sticky';
+
+export {
+  AboutPage,
+  App,
+  Card,
+  Footer,
+  InteractiveMap,
+  Intro,
+  LandingPage,
+  Routes,
+  Sticky,
+};
