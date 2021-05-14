6f49439e0025f23e412a8a7a4c0e0df0a5d46b47
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-05-13 20:42:24 -0600

    Refactor AnimalPage and update with hooks

--- src/containers/AnimalPage/AnimalPage.js
+++ src/containers/AnimalPage/AnimalPage.js
@@ -1,89 +1,92 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 
+import { LoadingContainer } from '../../components';
 import { animalDataCleaner, matchObjectInStore } from '../../helpers';
 
 import './AnimalPage.scss';
 
-export class AnimalPage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      animalData: {},
-    };
-  }
+export const AnimalPage = props => {
+  const [animalData, updateAnimalData] = useState(undefined);
+  const [isLoading, updateLoading] = useState(true);
 
-  componentWillMount() {
-    if (this.props.animals.length) {
+  useEffect(() => {
+    if (props.animals.length) {
       window.scrollTo(0, 0);
-      const animalData = matchObjectInStore(this.props, 'animal', 'animals');
-      this.setState({ animalData });
+      const animalData = matchObjectInStore(props, 'animal', 'animals');
+      updateAnimalData(animalData);
+      updateLoading(false);
     }
-  }
+  }, [props]);
 
-  componentWillReceiveProps(nextProps) {
-    window.scrollTo(0, 0);
-    const animalData = matchObjectInStore(nextProps, 'animal', 'animals');
-    this.setState({ animalData });
-  }
+  const renderAnimalStats = () => {
+    const cleanAnimalData = animalDataCleaner(animalData);
+    const animalProperties = Object.keys(cleanAnimalData).filter(
+      stat => cleanAnimalData[stat]
+    );
 
-  renderAnimalStats = () => {
-    const cleanAnimalData = animalDataCleaner(this.state.animalData);
-    const animalProperties = Object.keys(cleanAnimalData);
-    let longStats = [];
-    let basicStats = [];
-    animalProperties.forEach((property, index) => {
-      if (property === 'The Facts' || property === "Why I'm Important") {
-        longStats.push(
-          <span key={index} className="animal-stat">
-            <span className="stat-title">{property}</span>
-            <span className="stat-body">{cleanAnimalData[property]}</span>
-          </span>
-        );
-      } else if (cleanAnimalData[property] !== '') {
-        basicStats.push(
-          <span key={index} className="animal-stat">
-            <span className="stat-title">{property}:</span>
-            <span className="stat-body">{cleanAnimalData[property]}</span>
-          </span>
-        );
-      }
-    });
+    const { basicStats, longStats } = animalProperties.reduce(
+      (accumulator, property, index) => {
+        if (property === 'The Facts' || property === "Why I'm Important") {
+          return {
+            ...accumulator,
+            longStats: [
+              ...accumulator.longStats,
+              <span key={index} className="animal-stat">
+                <span className="stat-title">{property}</span>
+                <span className="stat-body">{cleanAnimalData[property]}</span>
+              </span>,
+            ],
+          };
+        }
+
+        return {
+          ...accumulator,
+          basicStats: [
+            ...accumulator.basicStats,
+            <span key={index} className="animal-stat">
+              <span className="stat-title">{property}:</span>
+              <span className="stat-body">{cleanAnimalData[property]}</span>
+            </span>,
+          ],
+        };
+      },
+      { basicStats: [], longStats: [] }
+    );
 
     return { basicStats, longStats };
   };
 
-  render() {
-    const { name, image } = this.state.animalData;
-    const { basicStats, longStats } = this.renderAnimalStats();
-
-    return (
-      <div className="AnimalPage buffer">
-        <span className="animal-name">{name}</span>
-        <div className="animal-data-container">
-          <div className="wrapper">
-            {image && (
-              <img
-                className="animal-image"
-                src={image}
-                alt={`${name}-endangered-species`}
-              />
-            )}
-            <div className="animal-basic-stats">{basicStats}</div>
-          </div>
-          <div className="animal-long-stats">{longStats}</div>
-        </div>
-      </div>
-    );
+  if (isLoading) {
+    return <LoadingContainer />;
   }
-}
 
-export const mapStateToProps = store => {
-  return {
-    animals: store.animals,
-  };
+  const { name, image } = animalData;
+  const { basicStats, longStats } = renderAnimalStats();
+  console.log(animalData);
+
+  return (
+    <div className="AnimalPage buffer">
+      <span className="animal-name">{name}</span>
+      <div className="animal-data-container">
+        <div className="wrapper">
+          <img
+            className="animal-image"
+            src={image}
+            alt={`${name}-endangered-species`}
+          />
+          <div className="animal-basic-stats">{basicStats}</div>
+        </div>
+        <div className="animal-long-stats">{longStats}</div>
+      </div>
+    </div>
+  );
 };
+
+export const mapStateToProps = store => ({
+  animals: store.animals,
+});
 
 export default connect(mapStateToProps, null)(AnimalPage);
 
