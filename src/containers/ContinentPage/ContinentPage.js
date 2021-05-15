343d8f5154207633380291ccad58e3f2b82b67f4
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-05-15 17:45:03 -0600

    Update ContinentPage as functional component with hooks

--- src/containers/ContinentPage/ContinentPage.js
+++ src/containers/ContinentPage/ContinentPage.js
@@ -1,84 +1,71 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 
 import CardContainer from '../CardContainer/CardContainer';
+import { LoadingContainer } from '../../components';
 import { matchObjectInStore } from '../../helpers';
 
 import './ContinentPage.scss';
 
-export class ContinentPage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      continentData: {},
-      animals: [],
-    };
-  }
+export const ContinentPage = props => {
+  const [continentData, updateContinentData] = useState([]);
+  const [animals, updateAnimals] = useState([]);
+  const [isLoading, updateLoading] = useState(true);
 
-  componentWillMount() {
-    if (this.props.continents.length) {
+  useEffect(() => {
+    console.log(props);
+    if (props.continents.length) {
       window.scrollTo(0, 0);
       const continentData = matchObjectInStore(
-        this.props,
+        props,
         'continent',
         'continents'
       );
-      const animals = this.matchContinentAnimals(continentData.id);
+      const animals = matchContinentAnimals(continentData.id);
 
-      this.setState({ continentData, animals });
+      updateContinentData(continentData);
+      updateAnimals(animals);
+      updateLoading(false);
     }
-  }
+  }, [props]);
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.continents.length) {
-      window.scrollTo(0, 0);
-      const continentData = matchObjectInStore(
-        nextProps,
-        'continent',
-        'continents'
-      );
-      const animals = this.matchContinentAnimals(continentData.id);
-
-      this.setState({ continentData, animals });
-    }
-  }
-
-  matchContinentAnimals = continentId => {
-    const animals = this.props.animals.filter(
+  const matchContinentAnimals = continentId => {
+    const animals = props.animals.filter(
       animal => animal.continent_id === continentId
     );
     return animals;
   };
 
-  render() {
-    const { name, bio, image } = this.state.continentData;
-    return (
-      <div className="ContinentPage buffer">
-        <span className="continent-name">{name}</span>
-        <div className="continent-data-container">
-          <div className="wrapper">
-            {image && (
-              <img className="continent-image" src={image} alt={`${name}`} />
-            )}
-            <div className="continent-bio">
-              <span className="bio-title">Bio</span>
-              <span className="bio-body">{bio}</span>
-            </div>
-          </div>
-          <CardContainer continentAnimals={this.state.animals} />
-        </div>
-      </div>
-    );
+  if (isLoading) {
+    return <LoadingContainer />;
   }
-}
 
-export const mapStateToProps = store => {
-  return {
-    animals: store.animals,
-    continents: store.continents,
-  };
+  const { name, bio, image } = continentData;
+
+  return (
+    <div className="ContinentPage buffer">
+      <span className="continent-name">{name}</span>
+      <div className="continent-data-container">
+        <div className="wrapper">
+          {image && (
+            <img className="continent-image" src={image} alt={`${name}`} />
+          )}
+          <div className="continent-bio">
+            <span className="bio-title">Bio</span>
+            <span className="bio-body">{bio}</span>
+          </div>
+        </div>
+        <CardContainer continentAnimals={animals} />
+      </div>
+    </div>
+  );
 };
+
+export const mapStateToProps = ({ animals, continents }) => ({
+  animals,
+  continents,
+});
 
 export default connect(mapStateToProps, null)(ContinentPage);
 
