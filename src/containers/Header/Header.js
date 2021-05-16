fe82f19e13c5bbf69f0a59360de5e3b2d56f50be
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-05-15 18:11:22 -0600

    Update Header as functional component with hooks

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,52 +1,47 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import * as actions from '../../actions';
 import PropTypes from 'prop-types';
+
+import * as actions from '../../actions';
+
 import './Header.scss';
 
-export class Header extends Component {
-  componentDidMount() {
-    this.props.populateData();
-  }
+export const Header = ({ location, populateData }) => {
+  useEffect(() => {
+    populateData();
+  }, []);
 
-  render() {
-    const { location } = this.props;
-    const aboutActive =
-      location === '/about/' ? 'nav-button active' : 'nav-button';
-    const homeActive = location === '/' ? 'nav-button active' : 'nav-button';
+  const aboutActive =
+    location === '/about/' ? 'nav-button active' : 'nav-button';
+  const homeActive = location === '/' ? 'nav-button active' : 'nav-button';
 
-    return (
-      <div className="Header">
-        <div className="wrapper">
-          <span className="headerLogo" />
-          <div className="nav-bar">
-            <NavLink exact to="/" className={homeActive}>
-              Home
-            </NavLink>
-            <NavLink exact to="/about" className={aboutActive}>
-              About
-            </NavLink>
-          </div>
+  return (
+    <div className="Header">
+      <div className="wrapper">
+        <span className="headerLogo" />
+        <div className="nav-bar">
+          <NavLink exact to="/" className={homeActive}>
+            Home
+          </NavLink>
+          <NavLink exact to="/about" className={aboutActive}>
+            About
+          </NavLink>
         </div>
       </div>
-    );
-  }
-}
-
-export const mapStateToProps = store => {
-  return {
-    animals: store.animals,
-  };
+    </div>
+  );
 };
 
-export const mapDispatchToProps = dispatch => {
-  return {
-    populateData: () => dispatch(actions.fetchAllData()),
-    resetAnimals: (inputValue, animals) =>
-      dispatch(actions.searchAnimals(inputValue, animals)),
-  };
-};
+export const mapStateToProps = ({ animals }) => ({
+  animals,
+});
+
+export const mapDispatchToProps = dispatch => ({
+  populateData: () => dispatch(actions.fetchAllData()),
+  resetAnimals: (inputValue, animals) =>
+    dispatch(actions.searchAnimals(inputValue, animals)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
 
