592485747d11e0c7477f3a79f6bb11414277a4bc
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-05-19 05:03:36 -0600

    Remove old tests from AnimalPage

--- src/containers/AnimalPage/AnimalPage.test.js
+++ src/containers/AnimalPage/AnimalPage.test.js
@@ -6,38 +6,15 @@ import mockProps from '../../data/mockProps';
 
 describe('AnimalPage tests', () => {
   let renderedAnimalPage;
-  let mockRenderAnimalStats;
 
   beforeEach(() => {
     renderedAnimalPage = shallow(
       <AnimalPage animals={mockProps.animals} match={mockProps.match} />
     );
-    mockRenderAnimalStats = renderedAnimalPage.instance().renderAnimalStats();
   });
 
   it('Should match the snapshot', () => {
     expect(renderedAnimalPage).toMatchSnapshot();
-  });
-
-  it('renderAnimalStats should return an object', () => {
-    expect(typeof mockRenderAnimalStats).toEqual('object');
-  });
-
-  it('renderAnimalStats should have an array for longStats and basicStats', () => {
-    expect(typeof mockRenderAnimalStats.basicStats).toEqual('object');
-    expect(typeof mockRenderAnimalStats.longStats).toEqual('object');
-  });
-
-  it('renderAnimalStats should not render empty properties', () => {
-    expect(renderedAnimalPage.state('animalData').population).toBeDefined;
-
-    renderedAnimalPage.state('animalData').population = '';
-    const mockRenderAnimalStats = renderedAnimalPage
-      .instance()
-      .renderAnimalStats();
-    const expected = undefined;
-
-    expect(mockRenderAnimalStats.basicStats.population).toEqual(expected);
   });
 
   describe('mapStateToProps tests', () => {
